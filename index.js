74250b928b793ef449965b7085188ab80f0c3feb
Billy Otieno Okendo <billyotieno7@gmail.com>
2022-06-09 00:29:28 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solutions in this file
+function writeCards(names, eventName) {
+
+    const messages = [];
+    
+     for (let count = 0; count < names.length; count++) {
+   
+       const message = `Thank you, ${names[count]}, for the wonderful ${eventName} gift!`;
+   
+       messages.push(message);
+     }
+     
+     return messages;
+   
+   }
+   
+   console.log(writeCards([], ''));
+   
+   function countDown(){
+       let countDown = 10;
+       while (countDown){
+           
+           console.log(countDown--);
+       }
+   console.log(countDown);
+   }
+   console.log(countDown());
